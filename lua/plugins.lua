6a523c141c173f663c35b7976e5cd152dfa94732
xiongjie <xiongjie.lambda@bytedance.com>
2022-03-16 00:09:28 +0800

    尝试 lua 配置 nvim

---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+-- vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- optional, for file icon
+		},
+		config = function() require'nvim-tree'.setup {} end
+	}
+
+end)
