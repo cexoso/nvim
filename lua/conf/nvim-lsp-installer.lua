0846a9970ddc472e4a9edc57a316067053f5e628
xiongjie <jiexiong@futunn.com>
2023-02-12 17:47:16 +0800

    rm sumneko_lua

--- lua/conf/nvim-lsp-installer.lua
+++ lua/conf/nvim-lsp-installer.lua
@@ -7,7 +7,7 @@ local lsp_installer_servers = require("nvim-lsp-installer.servers")
 -- 配置：https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local servers = {
     -- 语言服务器名称：配置选项
-    sumneko_lua = require("lsp.sumneko_lua"),
+    -- sumneko_lua = require("lsp.sumneko_lua"),
     -- pyright = require("lsp.pyright"),
     tsserver = require("lsp.tsserver"),
     -- html = require("lsp.html"),
