8f40938a127b343e5ca1be328c9d1d7de370f4aa
xiongjie <jiexiong@futunn.com>
2023-10-20 10:41:05 +0800

    chore: more space for nvim tree

--- lua/conf/nvim-tree.lua
+++ lua/conf/nvim-tree.lua
@@ -117,7 +117,7 @@ require("nvim-tree").setup { -- BEGIN_DEFAULT_OPTS
     centralize_selection = false,
     cursorline = true,
     debounce_delay = 15,
-    width = 30,
+    width = 40,
     side = "left",
     preserve_window_proportions = false,
     number = false,
