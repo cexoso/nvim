637aea5fc1f13413f83f885fab2e0347970a155a
xiongjie <jiexiong@futunn.com>
2024-01-03 21:35:41 +0800

    feat: only haici

--- lua/conf/vim-translator.lua
+++ lua/conf/vim-translator.lua
@@ -2,5 +2,5 @@ local map = require('keymap').map;
 
 map {'n', '<leader>t', ':TranslateW<cr>'}
 vim.cmd([[
-let g:translator_default_engines=['bing', 'google', 'haici', 'youdao']
+let g:translator_default_engines=['bing', 'haici']
 ]])
