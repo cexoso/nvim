f29ef4f024277780fa9ccea8f290e352b2ab7ede
xiongjie <jiexiong@futunn.com>
2024-07-31 15:10:32 +0800

    更新 cmp config，之前的有可能有报错的情况

--- lua/conf/nvim-cmp.lua
+++ lua/conf/nvim-cmp.lua
@@ -69,113 +69,94 @@ require('lspkind').init({
     },
 })
 
-local cmp = require("cmp")
+local cmp = require'cmp'
 
-cmp.setup(
-    ---@diagnostic disable-next-line: redundant-parameter
-    {
-        -- 指定补全引擎
-        snippet = {
-            expand = function(args)
-                -- 使用 vsnip 引擎
-                vim.fn["vsnip#anonymous"](args.body)
-            end
-        },
-        -- 指定补全源（安装了补全源插件就在这里指定）
-        sources = cmp.config.sources(
-            {
-                {name = "vsnip"},
-                {name = "nvim_lsp"},
-                {name = "path"},
-                {name = "buffer"},
-                {name = "cmdline"},
-                {name = "spell"},
-                {name = "cmp_tabnine"}
-            }
-        ),
-        -- 格式化补全菜单
-        formatting = {
-            format = lspkind.cmp_format(
-                {
-                    with_text = true,
-                    maxwidth = 50,
-                    before = function(entry, vim_item)
-                        vim_item.menu = "[" .. string.upper(entry.source.name) .. "]"
-                        return vim_item
-                    end
-                }
-            )
-        },
-        -- 对补全建议排序
-        sorting = {
-            comparators = {
-                cmp.config.compare.offset,
-                cmp.config.compare.exact,
-                cmp.config.compare.score,
-                cmp.config.compare.recently_used,
-                require("cmp-under-comparator").under,
-                require("cmp_tabnine.compare"),
-                cmp.config.compare.kind,
-                cmp.config.compare.sort_text,
-                cmp.config.compare.length,
-                cmp.config.compare.order
-            }
-        },
-        -- 绑定补全相关的按键
-        mapping = {
-            -- 上一个
-            ["<c-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-            ["<s-tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-            -- 下一个
-            ["<c-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-            ["<tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-            -- 选择补全
-            ["<CR>"] = cmp.mapping.confirm()
-            --  出现或关闭补全
-            -- ["<C-k>"] = cmp.mapping(
-            --     {
-            --         i = function()
-            --             if cmp.visible() then
-            --                 cmp.abort()
-            --             else
-            --                 cmp.complete()
-            --             end
-            --         end,
-            --         c = function()
-            --             if cmp.visible() then
-            --                 cmp.close()
-            --             else
-            --                 cmp.complete()
-            --             end
-            --         end
-            --     }
-            -- )
-        }
-    }
-)
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = {
+    -- 上一个
+    ["<c-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+    ["<s-tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+    -- 下一个
+    ["<c-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ["<tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    -- 选择补全
+    ["<CR>"] = cmp.mapping.confirm()
+    --  出现或关闭补全
+    -- ["<C-k>"] = cmp.mapping(
+    --     {
+    --         i = function()
+    --             if cmp.visible() then
+    --                 cmp.abort()
+    --             else
+    --                 cmp.complete()
+    --             end
+    --         end,
+    --         c = function()
+    --             if cmp.visible() then
+    --                 cmp.close()
+    --             else
+    --                 cmp.complete()
+    --             end
+    --         end
+    --     }
+    -- )
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
 
--- 命令行 / 模式提示
-cmp.setup.cmdline(
-    "/",
-    {
-        sources = {
-            {name = "buffer"}
-        }
-    }
-)
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+require("cmp_git").setup() ]]-- 
 
--- 命令行 : 模式提示
-cmp.setup.cmdline(
-    ":",
-    {
-        sources = cmp.config.sources(
-            {
-                {name = "path"}
-            },
-            {
-                {name = "cmdline"}
-            }
-        )
-    }
-)
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 vim.g.vsnip_snippet_dir = "~/.config/nvim/snippet";
