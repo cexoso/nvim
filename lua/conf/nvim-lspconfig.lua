ec8f7fe33e4e6f731e9b0ab1263699ed860ed07c
xiongjie <jiexiong@futunn.com>
2024-12-20 17:07:09 +0800

    解决 vue plugin 失效的问题，现在通过指明 location 解决

--- lua/conf/nvim-lspconfig.lua
+++ lua/conf/nvim-lspconfig.lua
@@ -7,6 +7,7 @@ lspconfig.ts_ls.setup {
     plugins = {
       {
         name = "@vue/typescript-plugin",
+        location = "~/node_modules/@vue/typescript-plugin",
         languages = {"vue"},
       },
     },
