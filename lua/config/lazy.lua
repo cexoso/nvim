cad30165537c4bfd9a2b16e26421728403f486f3
xiongjie <jiexiong@futunn.com>
2025-02-08 15:05:37 +0800

    disabled change detection

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,12 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    {
+      import = "plugins",
+      change_detection = {
+        enabled = false,
+      }
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
