9841e47d2cf5405ef45e713433f66a2de500d83e
xiongjie <jiexiong@futunn.com>
2025-02-10 10:40:19 +0800

    add hash

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -37,7 +37,7 @@ return {
       virt_text_priority = 100,
       use_focus = true,
     },
-    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+    current_line_blame_formatter = '<author>, <author_time:%R> - <summary> <abbrev_sha>',
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
