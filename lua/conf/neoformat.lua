e91ee5e96857335f98a9150f63f48f2776a735a1
jiexiong(熊杰) <jiexiong@futunn.com>
2026-06-12 08:28:35 +0800

    add oxfmt support with prettier fallback

--- lua/conf/neoformat.lua
+++ lua/conf/neoformat.lua
@@ -16,4 +16,29 @@ local map = require('keymap').map;
 
 vim.g.neoformat_try_node_exe = 1
 
+-- oxfmt: Prettier 兼容的高性能格式化器（JS/TS/JSON/YAML/TOML 等）
+-- https://oxc.rs/docs/guide/usage/formatter.html
+vim.g.neoformat_javascript_oxfmt = {
+  exe = 'oxfmt',
+  args = { '--stdin-filepath', '"%:p"' },
+  stdin = 1,
+  try_node_exe = 1,
+}
+vim.g.neoformat_typescript_oxfmt        = vim.g.neoformat_javascript_oxfmt
+vim.g.neoformat_javascriptreact_oxfmt   = vim.g.neoformat_javascript_oxfmt
+vim.g.neoformat_typescriptreact_oxfmt   = vim.g.neoformat_javascript_oxfmt
+vim.g.neoformat_json_oxfmt              = vim.g.neoformat_javascript_oxfmt
+vim.g.neoformat_jsonc_oxfmt             = vim.g.neoformat_javascript_oxfmt
+vim.g.neoformat_yaml_oxfmt              = vim.g.neoformat_javascript_oxfmt
+vim.g.neoformat_toml_oxfmt              = vim.g.neoformat_javascript_oxfmt
+
+-- 让 oxfmt 成为这些 filetype 的首选 formatter，找不到时 fallback 到 prettier
+vim.g.neoformat_enabled_javascript      = { 'oxfmt', 'prettier' }
+vim.g.neoformat_enabled_typescript      = { 'oxfmt', 'prettier' }
+vim.g.neoformat_enabled_javascriptreact = { 'oxfmt', 'prettier' }
+vim.g.neoformat_enabled_typescriptreact = { 'oxfmt', 'prettier' }
+vim.g.neoformat_enabled_json            = { 'oxfmt', 'prettier' }
+vim.g.neoformat_enabled_jsonc           = { 'oxfmt', 'prettier' }
+vim.g.neoformat_enabled_yaml            = { 'oxfmt', 'prettier' }
+
 map { "n", "<C-f>", "<cmd>Neoformat<CR>"}
